feeef68663f4063606f5c3d6cf2f777b70120ca9
Manicharan <manicharan150@gmail.com>
2024-12-27 10:05:06 +0530

    Last commit

---
+++ Javascript/two.js
@@ -0,0 +1,20 @@
+function Hello() {
+  console.log("Hello World");
+}
+
+function Hi() {
+  return "Hi everyone"
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+function buttonClick() {
+  alert("Button has been clicked")
+}
+
+Hello();
+console.log(Hi());
+console.log(sum(10, 20));
+console.log(sum(4, 5));
